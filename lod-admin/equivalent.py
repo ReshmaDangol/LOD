34736817bc7aa65277d7720c5eec7b4b99a495bf
reshma <resma.dangol@gmail.com>
2018-01-24 11:52:04 +0700

    >2 items in group

--- lod-admin/equivalent.py
+++ lod-admin/equivalent.py
@@ -1,10 +1,26 @@
 from common_functions import *
 
-
+classlist = []
 def modify_equiv_class():
     cursor = conn("equivalentclass").run()
     result = []
     for row in cursor:
+        c1 = row["c1"]
+        c2 = row["c2"]
+        id = row["id"]
+        classlist = []
+
+        search = conn("equivalentclass").filter(
+            (r.row["c1"] == c1) | (r.row["c1"] == c2) | (r.row["c2"] == c1) | (r.row["c2"] == c2)
+        ).filter(r.row["id"] != id).run()
+
+        for s in search:
+            classlist.append(s["c1"])
+            classlist.append(s["c2"])                    
+        
+        classlist = sorted(list(set(classlist)), key=str.lower)
+        groupname = "_".join(get_class_name(str(x)) for x in classlist)
+
         groupname = get_class_name(row["c1"]) + "-" + get_class_name(row["c2"])
         result.append({
             "class": row["c1"],
@@ -15,6 +31,8 @@ def modify_equiv_class():
             "group": groupname
         })
 
-    conn("equivalentclass_group").insert(result).run()
+    print(result)
+    # conn("equivalentclass_group").insert(result).run()
+
 
 modify_equiv_class()
