12d1aaf4688bdaba92568ac231f2618ff00de28e
reshma <resma.dangol@gmail.com>
2018-01-26 14:36:28 +0700

    get class detail-test

--- lod-admin/lod_api.py
+++ lod-admin/lod_api.py
@@ -428,12 +428,13 @@ def query_class_detail(s):
     for result in results:        
         p = result["p"]["value"]
         if(p == p_prev or p_prev == ''):
+            p_prev = result["p"]["value"]
             json_datatype.append(result["datatype"]["value"])
         else:
             p_prev = result["p"]["value"]
             json.append({
                 "p" : p_prev,
-                "datatype" : json_datatype
+                "datatype" : json_dtatype
             })
             json_datatype = []
 
