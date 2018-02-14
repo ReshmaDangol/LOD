36d5cb70af492da75e3f4bcc68fd815051016840
reshma <resma.dangol@gmail.com>
2018-02-14 19:24:07 +0700

    filter transitive subclass

--- lod-admin/preparedata.py
+++ lod-admin/preparedata.py
@@ -68,7 +68,7 @@ def prepare():
         is_subclass = conn("subclass").filter(
             lambda class_:
             class_["subclass"].default('foo').eq(c1)
-        ).filter({"transitive_subclass": "true"}).run()
+        ).filter({"transitive_subclass": "false"}).run()
 
         for s in is_subclass:
             result = {
@@ -130,7 +130,7 @@ def prepare():
         c = document["class"]
         id = document["id"]
         is_subclass = conn("subclass").filter(
-            lambda class_: class_["subclass"] == c).filter({"transitive_subclass": "true"}).count().run()
+            lambda class_: class_["subclass"] == c).filter({"transitive_subclass": "false"}).count().run()
         subclass = 1 if is_subclass > 0 else 0
 
         does_intersect = conn("intersection").filter(
