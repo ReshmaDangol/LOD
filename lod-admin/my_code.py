c160e5869c5e611ebea00ed8701c5b013698046c
reshma <resma.dangol@gmail.com>
2018-02-15 12:39:18 +0700

    undo transitive flag, insert in new table

--- lod-admin/my_code.py
+++ lod-admin/my_code.py
@@ -367,7 +367,7 @@ def subclass_check_transitivity():
                 print(get_class_name(c),get_class_name(sc),get_class_name(d["subclass"]) )
 
 
-    rows =  conn(tableprefix + "subclass").filter({"transitive_subclass": "true"}).not_().run()
+    rows =  conn(tableprefix + "subclass").filter({"transitive_subclass": "false"}).run()
     for row in rows:    
         conn(tableprefix + "subclass_graph").insert({
                     "class": c,
