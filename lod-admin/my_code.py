cb7014c32c5147bf3f51a9c41955f5c024084ab1
reshma <resma.dangol@gmail.com>
2018-02-25 10:51:16 +0700

    when c1==c2

--- lod-admin/my_code.py
+++ lod-admin/my_code.py
@@ -336,6 +336,7 @@ def fetch_property():
             print(class_arr[i])
             print(class_arr[j])
             poperty_between_class(class_arr[i], class_arr[j])
+            if(i!=j)
             poperty_between_class(class_arr[j], class_arr[i])
     return render_template("sparql.html")
 
